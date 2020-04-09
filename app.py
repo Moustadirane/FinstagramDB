146477f55ff6c6f76c15d3092d96bd7f0208ba68
Mousta Taenchiki <moustachfa269@gmail.com>
2020-04-08 22:05:38 -0400

    Updates

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
 import hashlib
+SALT = 'randomDatabases'
 
 #Initialize the app from Flask
 app = Flask(__name__)
@@ -21,6 +22,16 @@ conn = pymysql.connect(host='localhost',
 def hello():
     return render_template('index.html')
 
+
+@app.route('/home')
+def home():
+
+    user = session['username']
+
+    return render_template('home.html', username = user)
+    ##posts is some list of lists
+    ##home.html will iterate through all the data returned from our query
+
 #Define route for login
 @app.route('/login')
 def login():
@@ -36,80 +47,120 @@ def register():
 def loginAuth():
     #grabs information from the forms
     username = request.form['username']
-    password = request.form['password']
+    password = request.form['password'] + SALT
+    hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 
     #cursor used to send queries
     cursor = conn.cursor()
     #executes query
-    query = 'SELECT * FROM user WHERE username = %s and password = %s'
-    cursor.execute(query, (username, password))
-    #stores the results in a variable
+    query = 'SELECT * FROM Person WHERE username = %s and password = %s'
+
+
+    cursor.execute(query, (username, hashed_password))
+    ###stores the results in a variable
     data = cursor.fetchone()
-    #use fetchall() if you are expecting more than 1 data row
+
+    ###use fetchall() if you are expecting more than 1 data row
     cursor.close()
     error = None
     if(data):
         #creates a session for the the user
         #session is a built in
-        session['username'] = username
+        ##session['username'] = username
+        session['username'] = data['username']
         return redirect(url_for('home'))
     else:
         #returns an error message to the html page
-        error = 'Invalid login or username'
+        error = 'Invalid login or username. Please try again.'
         return render_template('login.html', error=error)
 
 #Authenticates the register
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
-    #grabs information from the forms
-    username = request.form['username']
-    password = request.form['password']
-
     #cursor used to send queries
     cursor = conn.cursor()
-    #executes query
-    query = 'SELECT * FROM user WHERE username = %s'
-    cursor.execute(query, (username))
-    #stores the results in a variable
-    data = cursor.fetchone()
-    #use fetchall() if you are expecting more than 1 data row
-    error = None
-    if(data):
-        #If the previous query returns data, then user exists
-        error = "This user already exists"
-        return render_template('register.html', error = error)
+
+    if(request.form):
+        username = request.form['username']
+        password = request.form['password'] + SALT
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        try:
+            query = 'INSERT INTO Person VALUES(%s,%s,%s,%s,%s)'
+            cursor.execute(query, (username, hashed_password, firstName, lastName, email))
+
+            if (cursor.fetchone()):
+                error = "This user already exists"
+                return render_template('register.html', error = error)
+            else:
+                conn.commit()
+                cursor.close()
+                return render_template('index.html')
+
+        except pymysql.err.IntegrityError:
+            error = "This username is taken."
+            return render_template('register.html', error = error)
     else:
-        ins = 'INSERT INTO user VALUES(%s, %s)'
-        cursor.execute(ins, (username, password))
-        conn.commit()
-        cursor.close()
-        return render_template('index.html')
+        error = "Some error occurred"
+        return render_template('register.html', error = error)
+
 
 @app.route('/logout')
 def logout():
     session.pop('username')
     return redirect('/')
 
+def run_sql_one(query, data):
+    cursor = conn.cursor()
+    cursor.execute(query, data)
+    data = cursor.fetchone()
+    cursor.close()
+    return data
 
 
 #Feature 1
+
 @app.route('/images')
 def show_images():
+    username = session['username']
+    cursor = conn.cursor()
 
     #Query to get all images a user has access to
     #Images that they POST
 
+    user_post = 'CREATE VIEW user_post as (SELECT DISTINCT pID, \
+    postingDate,filePath, allFollowers, caption, poster FROM Photo WHERE poster = %s) '
+    cursor.execute(user_post, (username))
+
     #Images that the people they are following POST
+    following_post = 'CREATE VIEW following_post as (SELECT DISTINCT pID, \
+    postingDate,filePath, allFollowers, caption, poster FROM Photo JOIN Follow\
+    ON (poster = followee) WHERE follower = %s AND followStatus = TRUE)'
+    cursor.execute(following_post,  (username))
 
     #Images that have been shared with them in a friend group
+    shared_image = 'CREATE VIEW shared_image as (SELECT DISTINCT pID, \
+    postingDate,filePath, allFollowers, caption, poster FROM Photo NATURAL JOIN SharedWith\
+    WHERE (%s, groupName) IN (SELECT username, groupName FROM BelongTo) )'
+    cursor.execute(shared_image, (username))
 
-    return render_template('images.html')
+    query = 'SELECT * FROM user_post UNION (SELECT * FROM following_post) \
+    UNION (SELECT * FROM shared_image) ORDER BY postingDate DESC'
+
+    cursor.execute(query)
+
+    info = cursor.fetchall()
+
+    cursor.close()
+
+    return render_template('images.html', data = info)
 
 #Feature 2
 
-
 #Feature 3
-
 
 #Feature 4
 
