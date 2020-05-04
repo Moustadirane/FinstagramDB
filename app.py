c90ad2ebb880c3be2520410c62f628ff3cae2860
Mousta Taenchiki <moustachfa269@gmail.com>
2020-05-03 23:29:08 -0400

    Part 4

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def run_sql_one(query, data):
     cursor.close()
     return data
 
-#Feature 1
+#Feature 1 & 2
 
 @app.route('/images')
 def show_images():
@@ -173,24 +173,162 @@ def show_images():
 
     return render_template('images.html', data = info, username = username )
 
-#Feature 2
-#
-#@app.route('/post', methods = ['GET', 'POST'])
-#def post_image():
-#    username = session['username']
-#    cursor = conn.cursor()
-#    photo = request.form['photo']
-#    query = 'INSERT INTO Photo(pID, postingDate,filePath, allFollowers, caption, poster) \
-#    VALUES(%s,%s, %s,%s,%s, %s)'
-#    cursor.execute(query(photo, username))
-#    conn.commit()
-#    cursor.close()
-#    return redirect(url_for('home'))
 #Feature 3
+@app.route('/makepost')
+def makepost():
+    return render_template('post.html')
+
+@app.route('/post', methods = ['GET', 'POST'])
+def post_image():
+    #variable names for photo table and cursor
+    username = session['username']
+    cursor = conn.cursor()
+
+    postingDate = dt.datetime.now()
+    postingDate.strftime( "%Y-%m-%d %H: %M: %S")
+    filePath = request.form['path']
+    allFollowers = request.form['allFollowerStatus']
+    caption = request.form['caption']
+
+    query = "SELECT MAX(pID) FROM Photo"
+    cursor.execute(query)
+
+    pID = int(cursor.fetchone()['MAX(pID)']) + 1
+
+    #photo = request.form['photo']
+    query = 'INSERT INTO Photo(pID, postingDate,filePath, allFollowers, caption, poster) \
+    VALUES(%s,%s, %s,%s,%s, %s)'
+    cursor.execute(query, (pID, postingDate, filePath, allFollowers, caption, username))
+    conn.commit()
+    cursor.close()
+    return redirect('/home')
 
 #Feature 4
+#Pending Requests from people to you
+
+@app.route('/pending_followrequests')
+def pendingRequests():
+    username = session['username']
+
+    cursor = conn.cursor
+    query = "SELECT follower FROM follow WHERE followee = %s AND followStatus = 0"
+
+    cursor.execute(query, (username))
+    data = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+
+    if (data):
+        return render_template('pendingFollowRequests.html')
+    else:
+        error = " There are no follow requests "
+        #goes back to home
+        return render_template("home.html", error = error)
+
+@app.route('/acceptRequests')
+def acceptFollowRequests():
+    username = session['username']
+
+    cursor = conn.cursor()
+
+    #pendingFollowRequests.html confirms request
+    currStatus = request.args['requests']
+
+    #write query to update database to true for a follow request
+    query = "UPDATE FOLLOW SET followStatus = 1 WHERE followee = %s AND follower = %s"
+
+    cursor.execute(query, (username, currStatus))
+    conn.commit()
+    cursor.close()
+    return redirect('/home')
+
+@app.route('/rejectRequests')
+def rejectFollowRequests():
+    username = session['username']
+
+    cursor = conn.cursor()
+
+    #pendingFollowRequests.html denies request
+    currStatus = request.args['requests']
+
+    #write query to update database to true for a follow request
+    query = "DELETE FROM FOLLOW WHERE followee = %s AND follower = %s"
+
+    cursor.execute(query, (username, currStatus))
+    conn.commit()
+    cursor.close()
+    return redirect('/home')
+
+@app.route('/followPeople')
+def followPeople():
+    return render_template("followOthers.html")
+
+@app.route('/followPeopleNow', methods = ['POST'])
+def followPeopleNow():
+    if (request.form):
+        cursor = conn.cursor()
+        followee = request.form['username']
+        follower = session['username']
+
+        query = "INSERT INTO Follow(follower, followee, followStatus) VALUES (%s,%s,0)"
+
+        cursor.execute(query, (follower,followee))
+        conn.commit()
+        cursor.close()
+        return redirect('/home')
+
+    else:
+        error = "Error Occured when trying to follow "
+        return render_template('home.html', error = error)
+
 
 #Feature 5
+@app.route('/make_friendgroup')
+def make_friendgroup():
+    return render_template('friendgroup.html')
+
+@app.route('/groupMaker', methods = ['POST'])
+def groupMaker():
+    if (request.form):
+        cursor = conn.cursor()
+
+        groupCreator = session['username']
+        description = request.form['description']
+        groupName = request.form['groupName']
+
+        if (isGroupUsed(groupName, groupCreator)):
+            error = "You already have a group with this name. "
+            return render_template('friendgroup.html', error = error)
+
+        else:
+            query = 'INSERT INTO FriendGroup(groupName, groupCreator, description) VALUES(%s,%s,%s)'
+            cursor.execute(query, (groupName, groupCreator, description))
+
+            #make sure to also place groupOwner in BelongTo
+            query = 'INSERT INTO BelongTo(username, groupName, groupCreator) VALUES(%s,%s,%s)'
+            cursor.execute(query,(groupCreator, groupName, groupCreator))
+
+            conn.commit()
+            cursor.close()
+            return redirect('/home')
+    else:
+        error = "Can't create a group"
+        return render_template("home.html", error = error)
+
+def isGroupUsed(groupName, groupCreator):
+    cursor = conn.cursor()
+
+    query = ' SELECT * FROM FriendGroup WHERE groupName = %s AND groupCreator = %s '
+
+    cursor.execute(query, (groupName, groupCreator))
+    data = cursor.fetchone()
+    cursor.close()
+
+    if (data):
+        return True
+    else:
+        return False
+
 
 app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
